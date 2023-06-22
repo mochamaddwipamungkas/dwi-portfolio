eacdaebbd4d6f99356a197a63a9f5f37b1730dbf
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-06-22 17:42:09 +0700

    ganti route detailproject10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Components from './components/components';
 import DetailProject from './components/detailproject';
 
@@ -8,12 +8,13 @@ import DetailProject from './components/detailproject';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/dwi-portfolio" element={<Components />} />
         <Route path="/detailproject" element={<DetailProject />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+
   )
 }
 export default App;
